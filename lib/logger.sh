fe2fc13319fab3317515224e436cbf49051cae02
Chris <vincentvancode101@gmail.com>
2025-02-10 22:16:19 +0100

    logger::log

--- lib/logger.sh
+++ lib/logger.sh
@@ -17,23 +17,15 @@ logger::setup_colors() {
         COLOR_RESET='\033[0m' # Reset text format
         RED='\033[0;31m'      # Red color
         GREEN='\033[0;32m'    # Green color
-        ORANGE='\033[0;33m'   # Orange color
-        BLUE='\033[0;34m'     # Blue color
         YELLOW='\033[0;33m'   # Yellow color
-        PURPLE='\033[0;35m'   # Purple color
         CYAN='\033[0;36m'     # Cyan color
-        YELLOW='\033[1;33m'   # Yellow color
     else
         # If colorization is disabled or unsupported, set variables to empty strings
         COLOR_RESET=''
         RED=''
         GREEN=''
-        ORANGE=''
-        BLUE=''
         YELLOW=''
-        PURPLE=''
         CYAN=''
-        YELLOW=''
     fi
 }
 
@@ -51,7 +43,7 @@ _log() {
     *) color="$COLOR_RESET" ;;
     esac
 
-    echo -e "${color}[$(now)] [$level]: $message${COLOR_RESET}"
+    echo -e "${color}[$(_now)] [$level]: $message${COLOR_RESET}"
 }
 
 logger::debug() {
@@ -92,37 +84,6 @@ logger::error() {
     fi
 }
 
-#-------------------------------------------------------------
-# PRINT MESSAGES
-# This function prints messages to standard error with support
-# for colorization if configured. It allows printing formatted
-# messages with colors and variables.
-#
-# Usage examples:
-#   msg "${RED}Read parameters:${COLOR_RESET}"
-#   msg "- flag: ${flag}"
-#   msg "- param: ${param}"
-#   msg "- arguments: ${args[*]-}"
-#
-# Arguments:
-#   $1: The message to be printed. It can include color codes
-#       or variables.
-#-------------------------------------------------------------
-msg() {
-    echo >&2 -e "${1-}"
-}
-
-#-------------------------------------------------------------
-# GET DATE AND TIME
-# This function retrieves the current date and time in the
-# format "YYYY-MM-DD-HH:MM:SS".
-#
-# Usage:
-#   now
-#
-# Returns:
-#   The current date and time.
-#-------------------------------------------------------------
-now() {
+_now() {
     date +%F-%T
 }
